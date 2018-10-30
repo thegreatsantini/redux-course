da3399a5f0ecabc37b1280ffad407ec075269525
thegreatsantini <lss13@my.fsu.edu>
2018-10-30 16:30:54 -0700

    setup reducers and actions

---
+++ Learn-Redux-Starter-Files/learn-redux/client/reducers/comments.js
@@ -0,0 +1,4 @@
+export default comments = (state = [], action) => {
+    console.log(state, action);
+    return state;
+}
---
+++ Learn-Redux-Starter-Files/learn-redux/client/reducers/posts.js
@@ -0,0 +1,4 @@
+export default posts = (state = [], action) => {
+    console.log(state, action);
+    return state;
+}
