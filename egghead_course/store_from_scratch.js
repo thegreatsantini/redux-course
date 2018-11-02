468a12edde663a0bdb5abc0767734991e3ed5926
thegreatsantini <lss13@my.fsu.edu>
2018-11-01 20:50:32 -0700

    implenented create store

--- egghead_course/store_from_scratch.js
+++ egghead_course/store_from_scratch.js
@@ -1,1 +1,47 @@
-// stuff
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+// this is the createStore that was shipped with redux!!
+const createStore = (reducer) => {
+    let state;
+    let listeners = [];
+
+    const getState = () => state;
+
+    const dispatch = (action) => {
+        state = reducer(state, action);
+        listeners.forEach(listener => listener());
+    };
+
+    const subscribe = (listener) => {
+        listeners.push(listener);
+        return () => {
+            listeners = listeners.filter(l => l !== listener);
+        };
+    };
+
+    dispatch({});
+
+    return { getState, dispatch, subscribe };
+};
+
+const store = createStore(counter);
+
+const render = () => {
+    document.body.innerText = store.getState();
+};
+
+store.subscribe(render);
+render();
+
+document.addEventListener('click', () => {
+    store.dispatch({ type: 'INCREMENT' });
+});
